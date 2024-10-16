38dc37ab296561a89b00a13535436d04ca406c4e
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-10-16 16:12:28 -0300

    Front Correccion UsersService

--- client/src/apis/users.services.jsx
+++ client/src/apis/users.services.jsx
@@ -1,17 +1,24 @@
 import axiosInstance from "../config/axiosInstance";
 import { useFetch } from "./hooks/useGet";
 
-export const useGetAssociates = (short = false) => {
-  if (short) {
-    const { data, isPending, isError, error } = useFetch(['associates'],"/v1/users/associatesselective")
-    const associates = data?.data || null;
-    return { data: associates, isPending, isError, error } 
-  } else {
-    const { data, isPending, isError, error } = useFetch(['associates'],"/v1/users/associates")
-    const associates = data?.data || null;
-    return { data: associates, isPending, isError, error } 
-  }
+export const useGetAssociates = () => {
+  const { data, isPending, isError, error } = useFetch(['associates'],"/v1/users/associates")
+  const associates = data?.data || null;
+  return { data: associates, isPending, isError, error } 
 }
+
+export const getShortAssociates = async () => {
+  try {
+    const response = await axiosInstance.get(`/v1/users/associatesselective`);
+    const associates = response.data?.data || null;
+    return associates;
+  } catch (error) {
+    throw new Response('Error al cargar los datos', {
+      status: 500,
+      statusText: error.message,
+    });
+  }
+};
 
 export const associateLoader = async (username) => {
   try {
