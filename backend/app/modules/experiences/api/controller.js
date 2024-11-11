b6d17c898770b223729046de42d68f55f28f7183
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-11-11 16:18:26 -0300

    Back Experience Query

--- backend/app/modules/experiences/api/controller.js
+++ backend/app/modules/experiences/api/controller.js
@@ -5,4 +5,29 @@ export default class Controller extends MongoController {
   constructor() {
     super(new Service);
   }
+
+  get = async (req, res, next) => {
+    try {
+      const filters = {};
+
+      // Recorrer las claves del query y generar el objeto filters
+      Object.keys(req.query).forEach((key) => {
+        if (req.query[key]) {  // Ignorar valores vacíos
+          if (['createdAt', 'updatedAt'].includes(key)) {
+            // Convertir los valores de fechas a objetos Date
+            filters[key] = new Date(req.query[key]);
+          } else {
+            // Para otros campos, simplemente los asignamos
+            filters[key] = req.query[key];
+          }
+        }
+      });
+      console.log(filters);
+      
+      const element = await this.service.get(filters);
+      res.sendSuccessOrNotFound(element);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
--- backend/app/modules/experiences/api/validation.js
+++ backend/app/modules/experiences/api/validation.js
@@ -1,12 +1,12 @@
 import { Joi, Segments } from 'celebrate';
 
 const validSchema = {
-  example: {
-    [Segments.BODY]: Joi.object().keys({
-      given_name: Joi.string().max(50).required(),
-      family_name: Joi.string().max(50).required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().min(6).required(),
+  get: {
+    [Segments.QUERY]: Joi.object().keys({
+      user: Joi.string().optional().hex().length(24),
+      _id: Joi.string().optional().hex().length(24),
+      position: Joi.string().optional(),
+      company: Joi.string().optional()
     }),
   },
 }
