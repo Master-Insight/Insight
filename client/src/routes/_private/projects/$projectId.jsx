a62b388e3f2c016ce5ea804e62220770ef3cf473
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-11-20 17:49:06 -0300

    Front Card Project

--- client/src/routes/_private/projects/$projectId.jsx
+++ client/src/routes/_private/projects/$projectId.jsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { getProject } from '../../../services/api/projects.services'
 import { useState } from 'react'
 import Frame from '../../../ui/boxes/Frame'
+import { useAppStore } from '../../../store/useAppStore'
 
 export const Route = createFileRoute('/_private/projects/$projectId')({
   loader: async ({ params }) => {
@@ -12,11 +13,30 @@ export const Route = createFileRoute('/_private/projects/$projectId')({
 
 function ThisProject () {
   const [project, setProject] = useState(Route.useLoaderData())
+  const { currentUser } = useAppStore();
+
   console.log(project);
   
+  const config = {
+    filters: [],
+    activeFilter: { },
+    fields: [],
+    card: <p>Card</p>,
+    currentUserId: currentUser._id,
+    actions: {},
+  }
+
   return (
     <Frame>
       <h2 className='text-3xl font-bold'>Proyecto: {project.title}</h2>
+
+
+
+
+
+
+
+
     </Frame>
   )
 }
